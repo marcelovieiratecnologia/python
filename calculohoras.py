69bd43e70505659931e5879118790b39f0a4d034
Marcelo Vieira <marcelovieiratecnologia@gmail.com>
2020-02-26 08:32:33 -0300

    alterações

--- calculohoras.py
+++ calculohoras.py
@@ -81,9 +81,10 @@ FEVEREIRO - 2020
 =>19/02/2020:  Entrada as '07:49:00' ---
 =>20/02/2020:  Entrada as '07:33:00' ---
 =>21/02/2020:  Entrada as '07:31:00' ---
+=>26/02/2020:  Entrada as '08:01:00' ---
 
                          -----------
-TOTAL ATÉ O MOMENTO DE: | 06:30:00   | ***## Anotações:    ##***
+TOTAL ATÉ O MOMENTO DE: | 06:59:00   | ***## Anotações:    ##***
                          -----------
 '''
 
@@ -99,7 +100,7 @@ h, m, s = (map(int, HrEntrada.split(':')))
 HrEntrada = datetime.timedelta(0,s,0,0,m,h)
 
 # Lista com Horários que ENTREI mais CEDO na Empresa
-listHrChegadas=['07:40:00','07:27:00','07:49:00','07:33:00','07:31:00']
+listHrChegadas=['07:40:00','07:27:00','07:49:00','07:33:00','07:31:00','08:01:00']
 
 
 HrSaida = '17:30:00' # Meu Horário de Saída
